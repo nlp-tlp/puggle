5829cbc7e2b2d6e48dac06f94d759849a2a66d7a
Michael Stewart <michael.stewart.webdev@gmail.com>
2023-11-19 14:48:17 +0800

    update word and sent limit

--- puggle/size_limits.py
+++ puggle/size_limits.py
@@ -2,7 +2,7 @@
 
 
 MAX_ROWS = 2000  # Maximum number of sents/rows
-MAX_SENT_LENGTH = 50  # Maximum words in each sentence
-MAX_WORD_LENGTH = 30  # Maximum length of each word
+MAX_SENT_LENGTH = 100  # Maximum words in each sentence
+MAX_WORD_LENGTH = 100  # Maximum length of each word
 
 MAX_CATEGORIES = 20  # Maximum number of categories in a categorical field.
