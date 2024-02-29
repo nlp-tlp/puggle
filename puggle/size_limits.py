7e8bf11de927644cf421cbe8711f98b8e31e6dd5
Michael Stewart <michael.stewart.webdev@gmail.com>
2024-02-29 12:25:40 +0800

    drastically increased size limits

--- puggle/size_limits.py
+++ puggle/size_limits.py
@@ -1,8 +1,8 @@
 """Size limit global variables."""
 
 
-MAX_ROWS = 2000  # Maximum number of sents/rows
-MAX_SENT_LENGTH = 100  # Maximum words in each sentence
+MAX_ROWS = 200000  # Maximum number of sents/rows
+MAX_SENT_LENGTH = 1000  # Maximum words in each sentence
 MAX_WORD_LENGTH = 100  # Maximum length of each word
 
-MAX_CATEGORIES = 20  # Maximum number of categories in a categorical field.
+MAX_CATEGORIES = 100  # Maximum number of categories in a categorical field.
